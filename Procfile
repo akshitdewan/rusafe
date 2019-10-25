947bfc82c545dd571ce13a067bb0d4aad0044d19
Akshit Dewan <akshit.dewan@yahoo.com>
2019-10-25 03:48:21 -0700

    addeed correct procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* Server
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* rusafe/Server
--- src/main/java/rusafe/Server.java
+++ src/main/java/rusafe/Server.java
@@ -17,10 +17,10 @@ public class Server {
 
   @SuppressWarnings("restriction")
 public static void main(String[] args) throws Exception {
-//	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-//    server.createContext("places", new MyHandler());
-//    server.setExecutor(null); // creates a default executor
-//    server.start();
+	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+    server.createContext("/places", new MyHandler());
+    server.setExecutor(null); // creates a default executor
+    server.start();
 	  System.out.println("hello");
   }
 
