659e41ca2b5ec58aef4bf3eff3f0c5f55cdb9b81
Akshit Dewan <akshit.dewan@yahoo.com>
2019-10-25 03:26:43 -0700

    init

---
+++ src/main/java/rusafe/Server.java
@@ -0,0 +1,37 @@
+package rusafe;
+
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+/*
+ * a simple static http server
+*/
+public class Server {
+
+  @SuppressWarnings("restriction")
+public static void main(String[] args) throws Exception {
+//	HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+//    server.createContext("places", new MyHandler());
+//    server.setExecutor(null); // creates a default executor
+//    server.start();
+	  System.out.println("hello");
+  }
+
+  @SuppressWarnings("restriction")
+static class MyHandler implements HttpHandler {
+    public void handle(HttpExchange t) throws IOException {
+      byte [] response = "Hello World".getBytes();
+      t.sendResponseHeaders(200, response.length);
+      OutputStream os = t.getResponseBody();
+      os.write(response);
+      os.close();
+    }
+  }
+}
